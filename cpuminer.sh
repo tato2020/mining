8c3c5f148b156f01486ae43e017f560280a66859
tato2020 <92654886+tato2020@users.noreply.github.com>
2022-01-11 19:46:13 +0100

    Create cpuminer.sh

---
+++ cpuminer.sh
@@ -0,0 +1,4 @@
+# cpuminer
+sudo apt update
+sudo apt-get install -y \build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev && \git clone https://github.com/M-Elghdban/cpuminer-opt-sugarchain.git && \cd cpuminer-opt-sugarchain && \./build-yespower.sh && \./cpuminer --cputest
+./cpuminer -a yespower -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qhp87peqyp7aef7uf4dj2s5uvr0vd5qlqtatdxp.Ubuntu-t4
